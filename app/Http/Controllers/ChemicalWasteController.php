6058e2e0bfc27d11f00db0ad4c4cfc192aac4897
Ezhekiel <lean.simulation.pwi2@gmail.com>
2026-08-14 09:34:05 +0700

    membuat configurasi database chemical waste

---
+++ app/Http/Controllers/ChemicalWasteController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\chemical___waste;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Storechemical___wasteRequest;
+use App\Http\Requests\Updatechemical___wasteRequest;
+
+class ChemicalWasteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storechemical___wasteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(chemical___waste $chemical___waste)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(chemical___waste $chemical___waste)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatechemical___wasteRequest $request, chemical___waste $chemical___waste)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(chemical___waste $chemical___waste)
+    {
+        //
+    }
+}
---
+++ database/factories/ChemicalWasteFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\chemical___waste;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<chemical___waste>
+ */
+class ChemicalWasteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
